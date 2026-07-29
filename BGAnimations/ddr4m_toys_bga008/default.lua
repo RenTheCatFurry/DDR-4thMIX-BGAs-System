84995355f6983f4371ec3aab7a832a58cc75d965
Ren the cat <156409463+RenTheCatFurry@users.noreply.github.com>
2026-07-28 23:21:15 -0400

    Update default.lua

--- BGAnimations/ddr4m_toys_bga008/default.lua
+++ BGAnimations/ddr4m_toys_bga008/default.lua
@@ -1,7 +1,7 @@
 local layer1 = {
     textures = {
         {
-            img = "toys/i88_toys invert 5x3 (stretch).png",
+            img = "toys/i88_toys 5x3 (stretch).png",
             size = {64,64},
             frames = {8,9,6,7},
             delay = 0,
@@ -16,7 +16,7 @@ local layer1 = {
 local layer2 = {
     textures = {
         {
-            img = "toys/i88_toys invert 5x3 (stretch).png",
+            img = "toys/i88_toys 5x3 (stretch).png",
             size = {64,64},
             frames = {9,6,7,8},
             delay = 0,
@@ -34,5 +34,14 @@ local layer2 = {
 
 return Def.ActorFrame{
     LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer1),
-    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2)
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2),
+
+    Def.Quad{
+        OnCommand = function(self)
+            self:zoomto(SCREEN_WIDTH, SCREEN_HEIGHT)
+            self:Center()
+            self:diffuse(1,1,1,1)
+            self:blend("InvertDest")
+        end
+    }
 }
