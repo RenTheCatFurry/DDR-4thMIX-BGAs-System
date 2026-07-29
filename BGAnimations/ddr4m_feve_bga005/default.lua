1a6bb5092d704217635c4e1d2d66786c19d38ef1
Ren the cat <156409463+RenTheCatFurry@users.noreply.github.com>
2026-07-28 23:25:55 -0400

    Update default.lua

--- BGAnimations/ddr4m_feve_bga005/default.lua
+++ BGAnimations/ddr4m_feve_bga005/default.lua
@@ -28,7 +28,50 @@ local layer2 = {
     coloranim = 1
 }
 
+local layer3 = {
+    textures = {
+        {
+        }
+    },
+    alpha = 0.5,
+    coloranim = 1,
+    properties = {
+        "invert"
+    }
+}
+
 return Def.ActorFrame{
+    -- config beat
+    OnCommand = function(self)
+        local start_beat = GAMESTATE:GetSongPosition():GetSongBeat()
+
+        self:SetUpdateFunction(function(self)
+            local beat = GAMESTATE:GetSongPosition():GetSongBeat()
+            beat = beat - start_beat
+            MESSAAGEMAN:Broadcast("Beat", {beat = beat})
+        end)
+    end,
+
+    -- layer 1
     LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer1),
-    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2)
+
+    -- layer 2
+    Def.ActorFrame{
+        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2),
+        BeatMessageCommand = function(self, params)
+            local beat = params.beat
+            beat = 1 - math.floor(beat % 2)
+            self:diffusealpha(beat)
+        end
+    },
+
+    -- layer 3
+    Def.ActorFrame{
+        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer3),
+        BeatMessageCommand = function(self, params)
+            local beat = params.beat
+            beat = math.floor(beat % 2)
+            self:diffusealpha(beat)
+        end
+    }
 }
