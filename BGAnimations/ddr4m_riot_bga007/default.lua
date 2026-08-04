38ef19490a509892c609a93530bf5729f881727d
Ren the cat <156409463+RenTheCatFurry@users.noreply.github.com>
2026-08-04 00:17:48 -0400

    Update default.lua

--- BGAnimations/ddr4m_riot_bga007/default.lua
+++ BGAnimations/ddr4m_riot_bga007/default.lua
@@ -3,18 +3,18 @@ local layer1 = {
         {
             img = "riot/dba_riot 2x2 (stretch).png",
             rgb = {1,0,0},
+            glow = {1,0,0,32/255},
             frames = {0}
         }
     },
     effect = "bgmirror2",
-    alpha = 0.5
+    alpha = 2/3
 }
 
 local layer2 = {
     textures = {
         {
             img = "riot/h97_riot 4x4 (stretch).png",
-            rgb = {1,0.5,0.5},
             size = {80,60},
             frames = {13,14,15},
             delay = 0,
@@ -23,12 +23,15 @@ local layer2 = {
             }
         }
     },
+    alpha = 0.75,
     blendmode = "Add",
     effect = "particlesin",
-    effectlength = 4
+    effectlength = 4,
+    spinlength = 3
 }
 
 return Def.ActorFrame{
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", {textures={{}},properties={"blackbg"}}),
     LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer1),
     LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2)
 }
