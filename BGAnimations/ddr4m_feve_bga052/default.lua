9b3f4dd415bb3b21543a0b4ec37ad893242dc0da
Ren the cat <156409463+RenTheCatFurry@users.noreply.github.com>
2026-08-07 00:46:10 -0400

    Add files via upload

---
+++ BGAnimations/ddr4m_feve_bga052/default.lua
@@ -0,0 +1,44 @@
+local layer1 = {
+    textures = {
+        {
+        }
+    },
+    effect = "bgdistort7"
+}
+
+local layer2 = {
+    textures = {
+        {
+            img = "feve/aba_feve f00 c00 4x4.png"
+        },
+        {
+            img = "feve/aba_feve f01 c00 4x4.png"
+        },
+        {
+            img = "feve/aba_feve f02 c00 4x4.png"
+        },
+        {
+            img = "feve/aba_feve f03 c00 4x4.png"
+        }
+    },
+    blendmode = "Add",
+    alpha = 0.6,
+    coloranim = 1,
+    properties = {
+        "coloramafix"
+    }
+}
+
+return Def.ActorFrame{
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer1),
+
+    Def.Quad{
+        OnCommand = function(self)
+            self:Center()
+            self:zoomto(SCREEN_WIDTH, SCREEN_HEIGHT)
+            self:diffuse(0,0,0,0.25)
+        end
+    },
+
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2)
+}
