2d3c1d13f1411ddb08aef90164ca53a27dbdab15
Ren the cat <156409463+RenTheCatFurry@users.noreply.github.com>
2026-08-12 05:02:10 -0400

    Update default.lua

--- BGAnimations/ddr4m_mero_custombga000/default.lua
+++ BGAnimations/ddr4m_mero_custombga000/default.lua
@@ -13,20 +13,7 @@ local layer2 = {
         {
             img = "mero/b97_mero 4x4 (stretch).png",
             size = {80,60},
-            frames = {8,9},
-            delay = 1,
-            properties = {"framesbeat"}
-        }
-    },
-    setmesh = {1,1}
-}
-
-local layer3 = {
-    textures = {
-        {
-            img = "mero/b97_mero 4x4 (stretch).png",
-            size = {80,60},
-            frames = {7,6},
+            frames = {8,8,9,9,6,7,6,7},
             delay = 0.5,
             properties = {"framesbeat"}
         }
@@ -34,47 +21,7 @@ local layer3 = {
     setmesh = {1,1}
 }
 
-local function ShowOn(...)
-    local visible = {}
-
-    for _, v in ipairs({...}) do
-        visible[v] = true
-    end
-
-    return function(self, params)
-        self:visible(visible[params.beat] or false)
-    end
-end
-
 return Def.ActorFrame{
-    OnCommand = function(self)
-        local start_beat = GAMESTATE:GetSongPosition():GetSongBeat()
-        local lastBeat = -1
-        local msg = {}
-
-        self:SetUpdateFunction(function(self)
-            local beat = math.floor(
-                (GAMESTATE:GetSongPosition():GetSongBeat() - start_beat)
-            )
-
-            if beat > 2 then beat = 2 end
-
-            if beat ~= lastBeat then
-                lastBeat = beat
-                msg.beat = beat
-                MESSAGEMAN:Broadcast("Keyframe", msg)
-            end
-        end)
-    end,
-
     LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer1),
-
-    Def.ActorFrame{
-        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2),
-        KeyframeMessageCommand = ShowOn(0,1)
-    },
-    Def.ActorFrame{
-        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer3),
-        KeyframeMessageCommand = ShowOn(2)
-    },
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2)
 }
