54189ab8875410d055f3dd22bcf443b0a93c8407
Ren the cat <156409463+RenTheCatFurry@users.noreply.github.com>
2026-08-20 23:13:46 -0400

    Add files via upload

---
+++ BGAnimations/ddr4m_toys_custombga002/default.lua
@@ -0,0 +1,28 @@
+local layer1 = {
+    textures = {
+        {
+            img = "toys/bdc_toys (stretch).png"
+        }
+    }
+}
+
+local layer2 = {
+    textures = {
+        {
+            img = "toys/i88_toys 5x3 (stretch).png",
+            size = {64,64},
+            frames = {9,6,7,8},
+            delay = 0,
+            properties = {
+                "framesseq"
+            }
+        }
+    },
+    effect = "snailout",
+    effectlength = 4
+}
+
+return Def.ActorFrame{
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer1),
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2)
+}
