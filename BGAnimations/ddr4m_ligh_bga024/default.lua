73a225ec19a4a2c32f4f2a6be2ec307957d4441b
Ren the cat <156409463+RenTheCatFurry@users.noreply.github.com>
2026-09-01 21:31:43 -0400

    Update default.lua

--- BGAnimations/ddr4m_ligh_bga024/default.lua
+++ BGAnimations/ddr4m_ligh_bga024/default.lua
@@ -17,105 +17,21 @@ local layer2 = {
         {
             img = "ligh/e99_ligh 4x3 (stretch).png",
             size = {80,80},
-            frames = {4}
+            frames = {4,0,3,1},
+            delay = 1,
+            properties = {
+                "framesbeat"
+            }
         }
     },
     effect = "snailin",
-    effectlength = 2
+    effectlength = 1,
+    properties = {
+        "effecthalf"
+    }
 }
-
-local layer3 = {
-    textures = {
-        {
-            img = "ligh/e99_ligh 4x3 (stretch).png",
-            size = {80,80},
-            frames = {0}
-        }
-    },
-    effect = "snailin",
-    effectlength = 2,
-    effectoffset = 3
-}
-
-local layer4 = {
-    textures = {
-        {
-            img = "ligh/e99_ligh 4x3 (stretch).png",
-            size = {80,80},
-            frames = {3}
-        }
-    },
-    effect = "snailin",
-    effectlength = 2,
-    effectoffset = 2
-}
-
-local layer5 = {
-    textures = {
-        {
-            img = "ligh/e99_ligh 4x3 (stretch).png",
-            size = {80,80},
-            frames = {1}
-        }
-    },
-    effect = "snailin",
-    effectlength = 2,
-    effectoffset = 1
-}
-
-local function ShowOn(...)
-    local mask = {}
-    for _, v in ipairs({...}) do
-        mask[v] = true
-    end
-
-    return function(self, params)
-        self:visible(mask[params.beat] or false)
-    end
-end
-
-local show_0 = ShowOn(0)
-local show_1 = ShowOn(1)
-local show_2 = ShowOn(2)
-local show_3 = ShowOn(3)
 
 return Def.ActorFrame{
-    OnCommand = function(self)
-        local start_beat = GAMESTATE:GetSongPosition():GetSongBeat()
-        local lastBeat = -1
-        local msg = { beat = 0 }
-
-        self:SetUpdateFunction(function(self)
-            local current_beat = GAMESTATE:GetSongPosition():GetSongBeat()
-            local beat = math.floor((current_beat - start_beat)) % 4
-
-            if beat ~= lastBeat then
-                lastBeat = beat
-                msg.beat = beat
-                MESSAGEMAN:Broadcast("Keyframe", msg)
-            end
-        end)
-    end,
-
     LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer1),
-
-    Def.ActorFrame{
-        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2),
-        KeyframeMessageCommand = show_0
-    },
-
-    Def.ActorFrame{
-        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer3),
-        KeyframeMessageCommand = show_1
-    },
-
-    Def.ActorFrame{
-        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer4),
-        KeyframeMessageCommand = show_2
-    },
-
-    Def.ActorFrame{
-        LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer5),
-        KeyframeMessageCommand = show_3
-    }
+    LoadActor("../_DDR_4thMIX_BGAs_System/layer.lua", layer2)
 }
